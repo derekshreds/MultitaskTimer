753f15a5c8dcb23fe672de09a26c5e36bfbfa918
Derek Morris <derekshreds@gmail.com>
2019-02-20 20:01:58 -0600

    initial commit

---
+++ MultitaskTimer/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MultitaskTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.close_button = new System.Windows.Forms.Button();
+            this.time_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // close_button
+            // 
+            this.close_button.FlatAppearance.BorderSize = 0;
+            this.close_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close_button.ForeColor = System.Drawing.Color.White;
+            this.close_button.Location = new System.Drawing.Point(143, 0);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(25, 25);
+            this.close_button.TabIndex = 0;
+            this.close_button.Text = "X";
+            this.close_button.UseVisualStyleBackColor = true;
+            // 
+            // time_label
+            // 
+            this.time_label.AutoSize = true;
+            this.time_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time_label.ForeColor = System.Drawing.Color.White;
+            this.time_label.Location = new System.Drawing.Point(30, 22);
+            this.time_label.Name = "time_label";
+            this.time_label.Size = new System.Drawing.Size(112, 42);
+            this.time_label.TabIndex = 1;
+            this.time_label.Text = "00.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(170, 88);
+            this.Controls.Add(this.time_label);
+            this.Controls.Add(this.close_button);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "MultitaskTimer";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button close_button;
+        private System.Windows.Forms.Label time_label;
+    }
+}
+
---
+++ MultitaskTimer/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MultitaskTimer
+{
+    public partial class Form1 : Form
+    {
+        float duration = 1200f;
+        DateTime start_time = DateTime.Now;
+        bool allow_dragging = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            close_button.MouseClick += (s, e) =>
+            {
+                Environment.Exit(0);
+            };
+
+            time_label.MouseDoubleClick += (s, e) =>
+            {
+                start_time = DateTime.Now;
+            };
+
+            MouseDown += (s, e) =>
+            {
+                allow_dragging = true;
+            };
+
+            MouseUp += (s, e) =>
+            {
+                allow_dragging = false;
+            };
+
+            MouseMove += (s, e) =>
+            {
+                if (allow_dragging)
+                {
+                    this.Location = new Point(Cursor.Position.X - Width / 2, Cursor.Position.Y - Height / 2);
+                }
+            };
+
+            time_label.MouseDown += (s, e) =>
+            {
+                allow_dragging = true;
+            };
+
+            time_label.MouseUp += (s, e) =>
+            {
+                allow_dragging = false;
+            };
+
+            time_label.MouseMove += (s, e) =>
+            {
+                if (allow_dragging)
+                {
+                    this.Location = new Point(Cursor.Position.X - Width / 2, Cursor.Position.Y - Height / 2);
+                }
+            };
+
+            try
+            {
+                if (File.Exists(Application.StartupPath + "/duration.txt"))
+                {
+                    var data = File.ReadAllText(Application.StartupPath + "/duration.txt").Trim();
+                    duration = float.Parse(data);
+                }
+            }
+            catch { }
+
+            Thread t = new Thread(() =>
+            {
+                while (true)
+                {
+                    var time_left = (duration - DateTime.Now.Subtract(start_time).TotalSeconds);
+                    int time_hour = (int)Math.Floor(time_left / 60f);
+                    int time_seconds = (int)(time_left % 60);
+                    var time_text = time_hour.ToString("00") + ":" + time_seconds.ToString("00");
+
+                    if (time_label.InvokeRequired)
+                    {
+                        time_label.Invoke(new Action(() =>
+                        {
+                            time_label.Text = time_text;
+                            Refresh();
+                        }));
+                    }
+                    else
+                    {
+                        time_label.Text = time_text;
+                        Refresh();
+                    }
+                    Thread.Sleep(100);
+                }
+            });
+            t.Start();
+        }
+    }
+}
